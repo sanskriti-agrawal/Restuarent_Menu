403c6930478e855b452043e799cd335de0d5f809
sanskriti-agrawal <sanskriti.agrawal_cs19@gla.ac.in>
2022-03-22 14:04:51 +0530

    Updates

---
+++ precache-manifest.010a4d98121fa4356c20061f9478afed.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0fda62f1dc29bae43039",
+    "url": "/Restuarent_Menu/static/css/main.efc28f29.chunk.css"
+  },
+  {
+    "revision": "0fda62f1dc29bae43039",
+    "url": "/Restuarent_Menu/static/js/main.0fda62f1.chunk.js"
+  },
+  {
+    "revision": "7434611c6f407bbd86a9",
+    "url": "/Restuarent_Menu/static/js/1.7434611c.chunk.js"
+  },
+  {
+    "revision": "5e337c4beb678fc3937e",
+    "url": "/Restuarent_Menu/static/js/runtime~main.5e337c4b.js"
+  },
+  {
+    "revision": "bc78bffcd3293446ca60fb878195f37b",
+    "url": "/Restuarent_Menu/index.html"
+  }
+];
