8f050f6e0bf4f07368cd9aaf8e669c5af30ba78e
Sanskriti Agrawal <60340843+sanskriti-agrawal@users.noreply.github.com>
2022-03-26 10:24:21 +0530

    Update menuApi.js

--- src/components/menuApi.js
+++ src/components/menuApi.js
@@ -20,7 +20,7 @@ const Menu = [
     },
     {
       id: 3,
-      image: "./images/corn.jpg",
+      image: "images/corn.jpg",
       name: "Corn",
       category: "breakfast",
       price: "10₹",
@@ -29,7 +29,7 @@ const Menu = [
     },
     {
       id: 4,
-      image: "../images/chola.jpg",
+      image: "images/chola.jpg",
       name: "Chola",
       category: "lunch",
       price: "50₹",
@@ -38,7 +38,7 @@ const Menu = [
     },
     {
       id: 5,
-      image: "../images/pizza.jpg",
+      image: "images/pizza.jpg",
       name: "Pizza",
       category: "evening",
       price: "80₹",
@@ -47,7 +47,7 @@ const Menu = [
     },
     {
       id: 6,
-      image: "../images/nonvegthali.jpg",
+      image: "images/nonvegthali.jpg",
       name: "Non-Veg Thali",
       category: "dinner",
       price: "180₹",
@@ -56,7 +56,7 @@ const Menu = [
     },
     {
       id: 7,
-      image: "../images/sweet.jpg",
+      image: "images/sweet.jpg",
       name: "Sweets",
       category: "dinner",
       price: "60₹",
@@ -65,7 +65,7 @@ const Menu = [
     },
     {
       id: 8,
-      image: "../images/rajmarice.jpg",
+      image: "images/rajmarice.jpg",
       name: "Rajma Rice",
       category: "lunch",
       price: "60₹",
@@ -74,7 +74,7 @@ const Menu = [
     },
     {
       id: 9,
-      image: "../images/samosa.jpg",
+      image: "images/samosa.jpg",
       name: "Samosa",
       category: "evening",
       price: "10₹",
